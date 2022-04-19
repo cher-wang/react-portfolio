9d9bb5e0c42e26fd7a1ee5e42fc87ae63ba38ead
Cheryl Wang <ziiyewang@gmail.com>
2022-04-19 16:53:16 +0900

    add tailwind and do some basic cleaning

---
+++ src/_app.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <header></header>
+      <main className="flex-1 overflow-x-hidden">something here</main>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;
